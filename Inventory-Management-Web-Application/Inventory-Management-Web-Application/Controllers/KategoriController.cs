ca4e2390c1623d77e6d001a30e3ad95e21aac5ee
mehmetozcelik <mehmetozcelik.4181@gmail.com>
2018-10-31 00:06:45 +0300

    Kategori İşlemleri

---
+++ Inventory-Management-Web-Application/Inventory-Management-Web-Application/Controllers/KategoriController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory_Management_Web_Application.Models;
+
+namespace Inventory_Management_Web_Application.Controllers
+{
+    public class KategoriController : Controller
+    {   
+        InventoryContext db = new InventoryContext();
+
+        // GET: Kategori
+        public ActionResult Listesi()
+        {
+            return View(db.Kategori.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View(db.Kategori.ToList());
+        }
+
+        public ActionResult Duzenle()
+        {
+            return View();
+        }
+
+    }
+}
