05fbeed83bd9bab6f3226306509903a1f1d81a61
Fatih <fatih47154@gmail.com>
2018-12-12 15:13:16 +0300

    Print Controller

--- Inventory-Management-Web-Application/Inventory-Management-Web-Application/Controllers/PrintController.cs
+++ Inventory-Management-Web-Application/Inventory-Management-Web-Application/Controllers/PrintController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Rotativa;
 
 namespace Inventory_Management_Web_Application.Controllers
 {
     public class PrintController : Controller
     {
-   
+
+        public ActionResult UrunCikis(int id)
+        {
+            var report = new ViewAsPdf("UrunCikis")
+                { };
+
+            return report;
+        }
     }
 }
