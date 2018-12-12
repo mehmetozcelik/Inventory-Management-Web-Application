f0a3a7500523c3ad66cd2903a3bb5da6d96b2ced
Hamza TAŞ <hamza.tas610@gmail.com>
2018-12-12 15:18:38 +0300

    print

--- Inventory-Management-Web-Application/Inventory-Management-Web-Application/Controllers/PrintController.cs
+++ Inventory-Management-Web-Application/Inventory-Management-Web-Application/Controllers/PrintController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Inventory_Management_Web_Application.Models;
 
 namespace Inventory_Management_Web_Application.Controllers
 {
     public class PrintController : Controller
     {
-   
+
+
     }
 }
